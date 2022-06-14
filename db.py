963e47dba7f1ae14e7cd52636e9a86b37c49435a
anbarasan-raji <59604636+anbarasan-raji@users.noreply.github.com>
2022-06-14 11:25:30 +0530

    databse connection

---
+++ db.py
@@ -0,0 +1,20 @@
+## Connecting to the database
+
+## importing 'mysql.connector' as mysql for convenient
+import mysql.connector as mysql
+
+## connecting to the database using 'connect()' method
+## it takes 3 required parameters 'host', 'user', 'password', 'database'
+conn = mysql.connect(
+        host = "localhost",
+        user = "root",
+        password = "12345",
+        database = "cposres_865211121_db"
+    )
+
+mycursor = conn.cursor()
+
+mycursor.execute("show databases")
+
+for i in mycursor:
+    print(i)
